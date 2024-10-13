8722460ac2cb502afa125e73301f5eaac14caa04
Hacha_tq55@student.itstep.org <Hacha_tq55@student.itstep.org>
2024-10-13 18:57:04 +0300

    Добавьте файлы проекта.

---
+++ Homework_NP_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Homework_NP_1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string servarAddress = "127.0.0.1";
+            int port = 8888;
+        
+            try
+            {
+                TcpClient client = new TcpClient(servarAddress, port);
+                Console.WriteLine("Подключение к серверу...");
+
+                NetworkStream stream = client.GetStream();
+                Console.WriteLine("Введите 'date', чтоб получить дату или 'time', чтоб получить время:");
+                string reguest = Console.ReadLine();
+
+                byte[] data = Encoding.ASCII.GetBytes(reguest);
+                stream.Write(data, 0, data.Length);
+
+                data = new byte[256];
+                StringBuilder response = new StringBuilder();
+                int bytes = stream.Read(data, 0, data.Length);
+                response.Append(Encoding.ASCII.GetString(data, 0, bytes));
+                Console.WriteLine($"Ответ сервера: {response}");
+
+                
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TcpServer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace TcpServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            StartServer();
+        }
+
+        public static void StartServer()
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 8888;
+
+            TcpListener server = new TcpListener(ipAddress, port);
+            server.Start();
+
+            Console.WriteLine("Сервер запущен...");
+
+   
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Kлиент подключен.");
+
+                NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[256];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                string reguest = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Запрос от клиента: { reguest}");
+
+                string response = "";
+
+                if (reguest.ToLower() == "date")
+                {
+                    response = DateTime.Now.ToShortDateString();
+                }
+                else if (reguest.ToLower() == "time")
+                {
+                    response = DateTime.Now.ToShortTimeString();
+                }
+                else
+                {
+                    Console.WriteLine("Неправильный запрос");
+                }
+
+                byte[] data = Encoding.ASCII.GetBytes(response);
+                stream.Write(data, 0, data.Length);
+
+                client.Close();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
